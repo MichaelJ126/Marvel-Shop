afb429a2c111759490a8a97436bd42264449f3e7
Michael Mass <michaelmass@Michaels-MacBook-Pro.local>
2023-12-19 23:17:11 -0600

    Marvel

---
+++ marvel_shop/blueprints/site/routes.py
@@ -0,0 +1,97 @@
+# from itertools import product
+from flask import Blueprint, redirect, render_template, request, flash
+from marvel_shop.models import Product, Customer, Order, db
+from marvel_shop.forms import ProductForm
+
+
+site = Blueprint('site', __name__, template_folder='site_templates' )
+
+
+@site.route('/')
+def shop():
+    allprods = Product.query.all() 
+    allcustomers = Customer.query.all()
+    allorders = Order.query.all()
+
+
+    shop_stats = {
+        'products' : len(allprods),
+        'sales' : sum([order.order_total for order in allorders]),  
+        'customers' : len(allcustomers)
+    }
+
+
+
+    our_class = "Marvel Shop"
+
+    return render_template('shop.html', shop=allprods, coolmessage = our_class, stats=shop_stats )
+
+@site.route('/shop/create', methods= ['GET', 'POST'])
+def create():
+
+    createform = ProductForm()
+
+    if request.method == 'POST' and createform.validate_on_submit():
+        name = createform.name.data
+        image = createform.image.data
+        description = createform.description.data
+        price = createform.price.data
+        quantity = createform.quantity.data 
+
+
+        product = Product(name, price, quantity, image, description)
+
+        db.session.add(product)
+        db.session.commit()
+        flash(f"You have successfully created product {name}", category='success')
+        return redirect('/')
+
+    elif request.method == 'POST':
+        flash("We were unable to process your request", category='warning')
+        return redirect('/shop/create')
+    
+    return render_template('create.html', form=createform )
+
+
+@site.route('/shop/update/<id>', methods=['GET', 'POST'])
+def update(id):
+
+    product = Product.query.get(id)
+    updateform = ProductForm()
+
+    if request.method == 'POST' and updateform.validate_on_submit():
+
+        product.name = updateform.name.data 
+        product.image = updateform.image.data 
+        product.description = updateform.description.data 
+        product.price = updateform.price.data 
+        product.quantity = updateform.quantity.data 
+
+        db.session.commit()
+
+        flash(f"You have successfully updated product {product.name}", category='success')
+        return redirect('/')
+    
+    elif request.method == 'POST':
+        flash("We were unable to process your request", category='warning')
+        return redirect('/')
+    
+    return render_template('update.html', form=updateform, product=product )
+
+
+@site.route('/shop/delete/<id>')
+def delete(id):
+
+    product = Product.query.get(id)
+    
+    db.session.delete(product)
+    db.session.commit()
+
+    return redirect('/')
+    
+
+
+    
+
+    
+
---
+++ marvel_shop/models.py
@@ -0,0 +1,195 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash 
+from flask_login import UserMixin, LoginManager
+from datetime import datetime 
+import uuid 
+from flask_marshmallow import Marshmallow
+from .helpers import get_image 
+
+
+
+db = SQLAlchemy()
+login_manager = LoginManager() 
+ma = Marshmallow() 
+
+@login_manager.user_loader
+def load_user(user_id):
+  
+    return User.query.get(user_id) 
+
+class User(db.Model, UserMixin): 
+    user_id = db.Column(db.String, primary_key=True)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    def __init__(self, username, email, password, first_name="", last_name=""):
+        self.user_id = self.set_id()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.email = email 
+        self.password = self.set_password(password) 
+
+
+
+    def set_id(self):
+        return str(uuid.uuid4()) 
+    
+
+    def get_id(self):
+        return str(self.user_id) 
+    
+    def set_password(self, password):
+        return generate_password_hash(password)
+    
+
+    def __repr__(self):
+        return f"<User: {self.username}>"
+
+
+class Product(db.Model):
+    prod_id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    image = db.Column(db.String)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    date_added = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __init__(self, name, price, quantity, image="", description=""):
+        self.prod_id = self.set_id()
+        self.name = name
+        self.image = self.set_image(image, name)
+        self.description = description
+        self.price = price
+        self.quantity = quantity 
+
+    
+    def set_id(self):
+        return str(uuid.uuid4())
+    
+
+    def set_image(self, image, name):
+        print('image', image)
+        if not image: 
+            print('we dont have an image')
+            image = get_image(name) 
+
+        return image
+    
+    def decrement_quantity(self, quantity):
+
+        self.quantity -= int(quantity)
+        return self.quantity
+    
+    def increment_quantity(self, quantity):
+
+        self.quantity += int(quantity)
+        return self.quantity 
+    
+
+    def __repr__(self):
+        return f"<Product: {self.name}>"
+    
+
+
+class Customer(db.Model):
+    cust_id = db.Column(db.String, primary_key=True)
+    date_created = db.Column(db.String, default = datetime.utcnow() )
+    prodord = db.relationship('ProdOrder', backref = 'customer', lazy=True) 
+
+    def __init__(self, cust_id):
+        self.cust_id = cust_id 
+
+
+    def __repr__(self):
+        return f"<Customer: {self.cust_id}>"
+    
+
+class ProdOrder(db.Model):
+    prodorder_id = db.Column(db.String, primary_key=True)
+    prod_id = db.Column(db.String, db.ForeignKey('product.prod_id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable = False)
+    price = db.Column(db.Numeric(precision = 10, scale = 2), nullable = False)
+    order_id = db.Column(db.String, db.ForeignKey('order.order_id'), nullable = False)
+    cust_id = db.Column(db.String, db.ForeignKey('customer.cust_id'), nullable = False)
+
+
+    def __init__(self, prod_id, quantity, price, order_id, cust_id):
+        self.prodorder_id = self.set_id()
+        self.prod_id = prod_id 
+        self.quantity = quantity 
+        self.price = self.set_price(quantity, price) 
+        self.order_id = order_id
+        self.cust_id = cust_id 
+
+
+    def set_id(self):
+        return str(uuid.uuid4())
+    
+
+
+    def set_price(self, quantity, price):
+
+        quantity = int(quantity)
+        price = float(price)
+
+        self.price = quantity * price 
+        return self.price
+    
+
+    def update_quantity(self, quantity):
+
+        self.quantity = int(quantity)
+        return self.quantity
+    
+
+class Order(db.Model):
+    order_id = db.Column(db.String, primary_key=True)
+    order_total = db.Column(db.Numeric(precision=10, scale=2), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow())
+    prodord = db.relationship('ProdOrder', backref = 'order', lazy=True) 
+
+
+    def __init__(self):
+        self.order_id = self.set_id()
+        self.order_total = 0.00
+
+    
+    def set_id(self):
+        return str(uuid.uuid4())
+    
+
+    def increment_ordertotal(self, price):
+
+        self.order_total = float(self.order_total) 
+        self.order_total += float(price)
+
+        return self.order_total
+    
+    def decrement_ordertotal(self, price):
+
+        self.order_total = float(self.order_total) 
+        self.order_total -= float(price)
+
+        return self.order_total
+    
+
+    def __repr__(self):
+        return f"<Order: {self.order_id}>"
+
+
+class ProductSchema(ma.Schema):
+
+    class Meta:
+        fields = ['prod_id', 'name', 'image', 'description', 'price', 'quantity']
+
+
+
+product_schema = ProductSchema() 
+products_schema = ProductSchema(many=True)
